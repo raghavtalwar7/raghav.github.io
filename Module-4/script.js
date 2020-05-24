0ecc247f5e991a2a7cbbe9f9f1211ef3cefb1969
raghavtalwar7 <41549534+raghavtalwar7@users.noreply.github.com>
2020-05-24 17:41:24 +0530

    Initial Commit

--- Module-4/script.js
+++ Module-4/script.js
@@ -1,6 +1,8 @@
 /*
 Solution of assignment 4:
+
 Expected output:
+
 Hello Yaakov
 Good Bye John
 Good Bye Jen
@@ -14,13 +16,13 @@ Good Bye Jim
 */
 
 (function() {
-   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-   for (var i = 0; i < names.length; i++) {
-       var firstLetter = names[i].charAt(0).toLowerCase();
-       if (firstLetter === 'j') {
-           byeSpeaker.speak(names[i]);
-       } else {
-           helloSpeaker.speak(names[i]);
-       }
-   }
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
